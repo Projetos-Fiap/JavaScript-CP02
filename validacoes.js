945856a5f1bd6eb9bc2a49c4071f7b48a51c54e3
vMaia <rm99658@fiap.com.br>
2023-05-15 22:46:06 -0300

    criando index, estilo e base do js

---
+++ validacoes.js
@@ -0,0 +1,56 @@
+function validaFirstName(){
+    let value = document.forms["form"]["firstName"].value;
+    let element = document.querySelector('.firstName');
+    if (value.length < 5){
+        document.getElementById("errorFirstName").innerHTML = "Mínimo de 5 dígitos";
+        element.style.cssText  = "border-style: solid;border-width: 3px;border-color: red;";
+
+    } else {
+        document.getElementById("errorFirstName").innerHTML = "";
+        element.style.cssText = ""
+    }
+}
+
+function validaSecondName(){
+    let value = document.forms["form"]["secondName"].value;
+    let element = document.querySelector('.secondName');
+    if (value.length < 5){
+        document.getElementById("errorSecondName").innerHTML = "Mínimo de 5 dígitos"
+        element.style.cssText  = "border-style: solid;border-width: 3px;border-color: red;";
+    } else {
+        document.getElementById("errorSecondName").innerHTML = ""
+        element.style.cssText = ""
+    }
+}
+
+function validaEmail(){
+    let value = document.forms["form"]["email"].value;
+    let element = document.querySelector('.secondName');
+    if (value.length < 5){
+        document.getElementById("errorEmail").innerHTML = "Mínimo de 5 dígitos"
+    } else if(!value.includes("@")){
+        document.getElementById("errorEmail").innerHTML = "O caracter @ é necessário"
+    } else {
+        document.getElementById("errorEmail").innerHTML = ""
+    }
+}
+
+function validaPassword(){
+    let value = document.forms["form"]["password"].value;
+    if(value.length < 6 || value.length > 8){
+        document.getElementById("errorPassword").innerHTML = "A senha deve ter entre 6 e 8 caracteres"
+    } else {
+        document.getElementById("errorPassword").innerHTML = ""
+    }
+}
+
+function validaConfirmPassword(){
+    let password = document.forms["form"]["password"].value;
+    let confirmPassword = document.forms["form"]["confirmPassword"].value;
+
+    if( password != confirmPassword){
+        document.getElementById("errorConfirmPassword").innerHTML = "As senhas devem ser iguais"
+    } else {
+        document.getElementById("errorConfirmPassword").innerHTML = ""
+    }
+}
